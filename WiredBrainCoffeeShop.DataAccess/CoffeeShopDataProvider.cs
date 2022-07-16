ce304166eef6db2dc60b684677f5277a52b41053
Mahsa Agharezaei <mahsa.agharezaee@gmail.com>
2022-07-17 00:31:31 +0430

    create project and class library

---
+++ WiredBrainCoffeeShop.DataAccess/CoffeeShopDataProvider.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using WiredBrainCoffeeShop.DataAccess.Models;
+
+
+
+namespace WiredBrainCoffeeShop.DataAccess
+{
+    public class CoffeeShopDataProvider
+    {
+        public IEnumerable<CoffeeShop> LoadCoffeeShops()
+        {
+            yield return new CoffeeShop { Location = "frankfurt", BeansInStockingInKg = 107 };
+            yield return new CoffeeShop { Location = "Munich", BeansInStockingInKg = 87 };
+            yield return new CoffeeShop { Location = "frieburge", BeansInStockingInKg = 67 };
+        }
+    }
+}
---
+++ WiredBrainCoffeeShop.DataAccess/Models/CoffeeShop.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WiredBrainCoffeeShop.DataAccess.Models
+{  //i was inter a command
+
+    public class CoffeeShop
+    {
+        public string Location { get; set; }
+        public int BeansInStockingInKg { get; set; }
+
+
+    }
+}
